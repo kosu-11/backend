97b338ce0ea62bdcfad279bad8648bea29d04a99
kosu-11 <kosu1628015@gmail.com>
2026-02-16 14:37:25 +0530

    new file added

--- assignment/assignment_3/practice_project_1/notes_server/server.js
+++ assignment/assignment_3/practice_project_1/notes_server/server.js
@@ -1,8 +1,16 @@
 const http =require("http");
+const fs =require("fs");
 
 const server = http.createServer((req, res) => {
+    const url =new URL(req.url,`http://${req.headers.host}`);
+const pathname = url.pathname;
+
+if (req.method ==="GET" && pathname ==="/") {
   res.writeHead(200, {"Content-Type":"text/plain" });
-  res.end("Server is running");
+  res.end("Welcome to Notes API");
+}
+ // res.writeHead(200, {"Content-Type":"text/plain" });
+ // res.end("Server is running");
 });
 
 server.listen(3000,() => {console.log("Server running on http://localhost:3000");
